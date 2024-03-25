e1a86a9e948b8ba59807e6e4c3c2efcdf2dbeafd
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-03-25 16:46:14 -0500

    Added Error handleing.

---
+++ src/App/HttpError.ts
@@ -0,0 +1,143 @@
+//https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+function getMessage(value:number):string {
+    if(value < 400)
+        return "Redirected";
+
+    if(value === 401)
+        return "Unauthorized";
+
+    if(value === 402)
+        return "Payment Required";
+
+    if(value === 403)
+        return "Forbidden";
+
+    if(value === 404)
+        return "Not Found";
+
+    if(value === 405)
+        return "Method Not Allowed";
+
+    if(value === 406)
+        return "Not Acceptable";
+
+    if(value === 407)
+        return "Proxy Authentication Required";
+
+    if(value === 408)
+        return "Request Timeout";
+
+    if(value === 409)
+        return "Conflict";
+
+    if(value === 410)
+        return "Gone";
+
+    if(value === 411)
+        return "Length Required";
+
+    if(value === 412)
+        return "Precondition Failed";
+
+    if(value === 413)
+        return "Payload Too Large";
+
+    if(value === 414)
+        return "URI Too Long";
+
+    if(value === 415)
+        return "Unsupported Media Type";
+
+    if(value === 416)
+        return "Range Not Satisfiable";
+
+    if(value === 417)
+        return "Expectation Failed";
+
+    if(value === 421)
+        return "Misdirected Request";
+
+    if(value === 422)
+        return "Unprocessable Content";
+
+    if(value === 423)
+        return "Locked";
+
+    if(value === 424)
+        return "Failed Dependency";
+
+    if(value === 425)
+        return "Too Early";
+
+    if(value === 426)
+        return "Upgrade Required";
+
+    if(value === 428)
+        return "Precondition Required";
+
+    if(value === 429)
+        return "Too Many Requests";
+
+    if(value === 431)
+        return "Request Header Fields Too Large";
+
+    if(value === 451)
+        return "Unable for Legal Reasons";
+
+    if(value < 500)
+        return "Bad Request";
+
+    if(value === 501)
+        return "Not Implemented";
+
+    if(value === 502)
+        return "Bad Gateway";
+
+    if(value === 503)
+        return "Service Unabailable";
+
+    if(value === 504)
+        return "Gateway Timeout";
+
+    if(value === 505)
+        return "HTTP Version Not Supported";
+
+    if(value === 506)
+        return "Variant Also Negotiates";
+
+    if(value === 507)
+        return "Insufficient Storage";
+
+    if(value === 508)
+        return "Loop Detected";
+
+    if(value === 510)
+        return "Not Extended";
+
+    if(value === 511)
+        return "Netowrk Authentication Required"
+
+    return "Internal Server Error";
+}
+
+export default class HttpError extends Error{
+    #status:number;
+
+    constructor(status:number, message?:string){
+        let value:number = Number(status);
+
+        if(isNaN(value)) {
+            message = "Invalid status: " + status
+            status = 500;
+        }else if(value < 300 || value > 599) {
+            message = "Status is not an error!"
+        }
+
+        super(message || getMessage(value));
+        this.#status = value;
+    }
+
+    get status():number{
+        return this.#status;
+    }
+}
--- src/App/index.ts
+++ src/App/index.ts
@@ -1,17 +1,59 @@
 import Route from "./Router/Route";
 import {IncomingMessage, ServerResponse} from "http";
 import Context from "./Context";
+import {Handler} from "./Router/Layer";
+import HttpError from "./HttpError";
 
 type Engine = (incoming:IncomingMessage, response:ServerResponse) => void;
 
+export type ErrorHandler = (err:any, ctx:Context)=>Promise<void>|void;
+
 export default class App extends Route{
     #engine:Engine;
+    #notFound:Handler;
+    #errorHandler:ErrorHandler;
 
     constructor(){
         super(undefined, undefined);
+
         this.#engine = (incoming:IncomingMessage, response:ServerResponse) => {
-            this.handle(new Context(incoming, response))
+            const ctx = new Context(incoming, response)
+            this.handle(ctx)
+                .then(()=>ctx.done())
+                .catch((err)=>{
+                    if(typeof err === "number")
+                        err = new HttpError(err);
+                    this.#errorHandler(err, ctx);
+                })
         }
+
+        this.#notFound = (ctx:Context) => {throw 404};
+        this.#errorHandler = (err:any, ctx:Context) => {
+            const {
+                status = 500,
+                message = err || "An Unknown Error Occured!"
+            } = err;
+            console.error(err);
+            ctx.status(status).write(message).done();
+        }
+    }
+
+    async handle(context:Context){
+        await super.handle(context)
+        if( !context.response.headersSent )
+            this.#notFound(context);
+    }
+
+    notFound(callback:Handler){
+        if(typeof callback !== "function")
+            throw new TypeError("Handler must be a function!");
+        this.#notFound = callback;
+    }
+
+    errorHandler(callback:ErrorHandler){
+        if(typeof callback !== "function")
+            throw new TypeError("Error Handler must be a function!");
+        this.#errorHandler = callback;
     }
 
     get engine():Engine{
