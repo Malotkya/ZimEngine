02687f305579c822c1e3a1c89e0cfa0e0466859d
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-03-25 16:45:32 -0500

    Added some getters/setters;

--- src/App/Context.ts
+++ src/App/Context.ts
@@ -9,13 +9,18 @@ export default class Context{
     #params:Dictionary<string>;
 
     constructor(request: Request, response: Response){
+
+        //Getter Only Variables
         this.#request = request;
         this.#response = response;
         this.#url = new URL(request.url || "", `http://${request.headers.host}`);
 
+        //Params Search Values
         this.#search = {};
         for(const [name, value] of this.#url.searchParams.entries())
             this.#search[name] = value;
+
+        //Defaults
         this.#params = Object.create(this.#search);
     }
 
@@ -45,5 +50,45 @@ export default class Context{
 
     get params():Dictionary<string>{
         return this.#params;
+    }
+
+    status(value:number):Context {
+        if(typeof value !== "number") {
+            value = Number(value);
+        }
+
+        if(isNaN(value)) {
+            throw new TypeError("Status code must be a nubmer!")
+        } else if( value < 100 || value > 999) {
+            throw new TypeError("Status code is out of range!");
+        }
+        
+        this.#response.statusCode = value;
+        return this;
+    }
+
+    json(object:Object){
+        if (!this.#response.getHeader("Content-Type")) {
+            this.#response.setHeader('Content-Type', 'application/json');
+        }
+        this.#response.write(JSON.stringify(object));
+        return this;
+    }
+
+    text(value:string){
+        if (!this.#response.getHeader("Content-Type")) {
+            this.#response.setHeader('Content-Type', 'application/text');
+        }
+        this.#response.write(value);
+        return this;
+    }
+
+    write(chunk:any){
+        this.#response.write(chunk);
+        return this;
+    }
+
+    done() {
+        this.#response.end();
     }
 }
