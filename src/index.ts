1cadab27ad08cffe804f4d5d51d4075ec053e074
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-03-25 16:47:20 -0500

    Exporting HttpError.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import App from "./App";
 import Context, {Request, Response} from "./App/Context";
 import Router from "./App/Router";
+import HttpError from "./App/HttpError";
 
-export {App, Context, Request, Response, Router};
+export {App, Context, Request, Response, Router, HttpError};
