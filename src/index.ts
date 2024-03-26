9f374c42b1a35e44743bb950eac970d6787053fa
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-03-26 12:29:30 -0500

    Exporting Route.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import App from "./App";
 import Context, {Request, Response} from "./Context";
 import Router from "./Router";
+import Route from "./Router/Route";
 import HttpError from "./HttpError";
 
-export {App, Context, Request, Response, Router, HttpError};
+export {App, Context, Request, Response, Router, HttpError, Route};
