7ca86fbcbcd2d955dd0ff2649d7b877fe58c788a
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-03-28 13:06:44 -0500

    Exporting View from Project.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import Context, {Request, Response} from "./Context";
 import Router from "./Router";
 import Route from "./Router/Route";
 import HttpError from "./HttpError";
+import View from "./View";
 
-export {App, Context, Request, Response, Router, HttpError, Route};
+module.exports = {App, Context, Request, Response, Router, HttpError, Route, View};
