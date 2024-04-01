3711adba3fc642e559a9d3b76e952e53364ada19
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-04-01 09:56:07 -0500

    removed join as it is unused.

--- src/Util.ts
+++ src/Util.ts
@@ -12,14 +12,3 @@ export function sleep(n:number=1):Promise<void>{
         setTimeout(res, n)
     });
 }
-
-/** Join Paths
- * 
- * @param {Array<string>} paths 
- * @returns {string}
- */
-export function join(...paths:Array<string>):string{
-    return paths.join("/")
-        .replace(/\/+/gm, "/") //Remove any multiple slashes
-        .replace(/\/$/, "");   //Remove trailing slash
-}
