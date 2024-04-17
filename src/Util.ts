1c28ddaabf105212c9d66125ab4e47acd36ebba1
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-04-17 11:26:29 -0500

    Added some types to util.

--- src/Util.ts
+++ src/Util.ts
@@ -3,6 +3,16 @@
  * @author Alex Malotky 
  */
 
+export type HeadersInit = Data<string>
+
+export interface Data<t> {
+    get(name:string):t|undefined,
+    set(name:string, value:t):void
+    delete(name:string):void
+    entries():IterableIterator<[string, t]>
+    [name:string]:any;
+}
+
 /** Sleep
  * 
  * @param {number} n 
@@ -26,4 +36,20 @@ export function dictionaryInclude(dictionary:Dictionary<any>, name:string):boole
     }
 
     return false;
+}
+
+/** Get File
+ * 
+ * Different IMplementation depending on environment.
+ * 
+ * @param {string} name 
+ * @returns {string}
+ */
+export async function getFile(name:string):Promise<string> {
+    if((typeof process !== 'undefined') && (process.release.name === 'node')){
+        const fs = await import("node:fs");
+        return fs.readFileSync(name).toString();
+    }
+
+    return require(name);
 }
