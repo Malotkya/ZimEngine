f6fca4a48197bcba7c597c0b6c2a8d6b423e75e8
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-10-25 15:14:54 -0500

    Made changes to Route.test

--- test/Route.test.js
+++ test/Route.test.js
@@ -1,17 +1,18 @@
-const {App, Router} = require("../lib");
-const {default:Route} = require("../lib/Router/Route");
-const {default:Layer} = require("../lib/Router/Layer");
+const {default:App, Router} = require("../lib");
+const {default:Route} = require("../lib/Routing/Route");
+const {default:Layer} = require("../lib/Routing/Layer");
 const request = require('supertest');
 
 const app = new App();
 const route = new Route();
 const home = new Router();
 
-app.use("/Go", new Route("/Really", undefined, [
-    new Layer("/Deep", undefined, (ctx)=>{
-        ctx.text("Good Job!");
-    })
-]))
+
+const Really = new Route("/Really");
+Really.use(new Layer("/Deep", (ctx)=>{
+    ctx.text("Good Job!");
+}));
+app.use("/Go", Really);
 
 const message = {
     value:"Hello World!"
@@ -23,34 +24,32 @@ home.all((ctx)=>{
 
 app.use("/", home);
 
-app.use("/:one/:two/:three/:four?", (ctx)=>{
+app.use("/:one/:two/:three", (ctx)=>{
     if(ctx.params["one"] === "Go")
         return; //Im just lazy here.
 
     const params = [
-        ctx.params["one"],
-        ctx.params["two"],
-        ctx.params["three"],
-        ctx.params["four"]
+        ctx.params.get("one"),
+        ctx.params.get("two"),
+        ctx.params.get("three")
     ];
 
     const search = [
-        ctx.search["one"],
-        ctx.search["two"],
-        ctx.search["three"]
+        ctx.search.get("one"),
+        ctx.search.get("two"),
+        ctx.search.get("three")
     ];
 
     ctx.json({params, search});
 });
 
 route.use("/Hello", 
-    (ctx)=>ctx.text("Hello World\n"),
-    (ctx)=>ctx.text("Hola Mundo\n")
+    (ctx)=>ctx.text("Hello World\n")
 );
 app.use(route);
 
 test("Init Test", done => {
-    request(app.engine)
+    request(app.server)
         .get("/")
         .expect("Content-Type", "application/json")
         .expect(message)
@@ -72,31 +71,28 @@ test("Params & Search Variables", done => {
 
     const uri = `/${params.join("/")}?one=${search[0]}&two=${search[1]}&three=${search[2]}`;
 
-    params.push(null);
-
-    request(app.engine)
+    request(app.server)
         .get(uri)
         .expect({params, search})
         .expect(200, done);
 });
 
-test("Complex Routing Test", done => {
-    request(app.engine)
+test("Simple Routing Test", done => {
+    request(app.server)
         .get("/Hello")
         .expect("Content-Type", "text/plain")
-        .expect("Hello World\nHola Mundo\n")
+        .expect("Hello World\n")
         .expect(200, done);
 })
 
 test("404 Test", done => {
-    request(app.engine)
+    request(app.server)
         .get("/Bad/Link")
         .expect(404, done);
 });
 
-// TODO: Make this work!
 test("Deep Router Test", done=>{
-    request(app.engine)
+    request(app.server)
         .get("/Go/Really/Deep")
         .expect("Content-Type", "text/plain")
         .expect("Good Job!")
