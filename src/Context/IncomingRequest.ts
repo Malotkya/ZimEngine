110470b52faf6e3c1230e14ac2fc68546a1c4c99
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-10-25 11:06:18 -0500

    Created Headers class to make checking headers easier accross systems.

--- src/Context/IncomingRequest.ts
+++ src/Context/IncomingRequest.ts
@@ -3,13 +3,23 @@ import { isCloudflareRequest } from "../Util";
 
 export type {IncomingMessage as NodeRequeset};
 
+class Headers extends Map<string, string> {
+    set(key:string, value:string){
+        return super.set(key.toLocaleLowerCase(), value);
+    }
+
+    get(key:string){
+        return super.get(key.toLocaleLowerCase());
+    }
+}
+
 export default class IncomingRequest {
-    private _headers:Map<string, string>;
+    private _headers:Headers;
     private _url:string;
     private _method:string;
 
     constructor(message: IncomingMessage|Request) {
-        this._headers = new Map();
+        this._headers = new Headers();
         this._url = message.url!;
         this._method = message.method!.toUpperCase();
 
