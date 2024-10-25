6563f35a195e9681275ce0c8df41bcc719e1af50
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-10-25 11:00:33 -0500

    Re add/wrote Static file middleware.

---
+++ src/Static.ts
@@ -0,0 +1,37 @@
+import { inCloudfareWorker } from "./Util";
+import MimeTypes from "./MimeTypes";
+import Context from "./Context";
+
+export default function Static(dir:string) {
+    if(inCloudfareWorker())
+        throw new Error("Cannot use static in cloudflare worker!");
+
+    const fs = require(/*webpackIgnore: true*/"node:fs");
+    const path = require(/*webpackIgnore: true*/"node:path");
+
+    return async function handleStaticFile(ctx:Context){
+        if(ctx.method !== "GET" && ctx.method !== "HEAD") {
+            return;
+        }
+
+        const target:string = path.join(dir, ctx.url.pathname);
+
+        if( fs.existsSync(target) === false) {
+            return;
+        }
+
+        const stats = fs.statSync(target);
+        if(stats.isDirectory()) {
+            return;
+        }
+
+        ctx.response.headers.set('Content-Type', MimeTypes(target));
+        ctx.response.headers.set("Content-Length", stats.size.toString());
+        ctx.response.headers.set('Content-Security-Policy', "default-src 'none'");
+        ctx.response.headers.set('X-Content-Type-Options', "nosniff");
+
+        fs.createReadStream(target)
+            .pipe(ctx.response)
+            .on("close", ()=>ctx.end());
+    }
+}
