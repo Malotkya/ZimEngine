3dc60f346d3ecff0d28d9733ab542ee4cc7579c9
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-10-25 09:15:11 -0500

    Combined ProtoResponse and OutgoingResponse.

--- src/Context/OutgoingResponse.ts
+++ src/Context/OutgoingResponse.ts
@@ -1,101 +1,177 @@
+/** Engine/Context/ProtoResponse
+ * 
+ */
 import { ServerResponse } from "node:http";
-import {HeadersInit} from "../Util";
+import {Transform, TransformCallback} from "node:stream";
+import { sleep } from "../Util";
 
-export type ResponseInit = [string, {status:number, headers:HeadersInit}, string|undefined];
+export type {ServerResponse as NodeResponse}
 
-export default class OutgoingResponse {
-    _server:ServerResponse|undefined;
-    _status:number|undefined;
-    _headers:HeadersInit;
-    _body:string;
-    _redirect:string|undefined;
-    
-    constructor(response?:ServerResponse) {
-        this._server = response;
-        this._headers = new Map();
-        this._body = "";
-    }
+export default class OutgoingResponse extends Transform{
+    //Strict private members
+    #server:ServerResponse|undefined;
+    #status:number;
+    #headers:Map<string, string>;
+    #body: Array<Uint8Array>;
+    #redirect: URL|undefined;
 
-    getHeader(name:string):string|undefined {
-        if(this._server) {
-            let temp = this._server.getHeader(name);
-            if(temp)
-                return String(temp);
-        }
+    //Not Strict
+    private working:boolean|undefined;
+    private static encoder = new TextEncoder();
 
-        return this._headers.get(name);
-    }
-
-    setHeader(name:string, value:string){
-        if(this._server){
-            this._server.setHeader(name, value);
-        } else {
-            this._headers.set(name, value);
-        }
-    }
-
-    get status():number {
-        if(this._server)
-            return this._server.statusCode;
-
-        return this._status || 200;
-    }
-
-    set status(value:number){
-        if(typeof value !== "number")
-            throw new TypeError("Status must be a number!");
-
-        if(this._server) {
-            this._server.statusCode = value;
-        } else {
-            this._status = value;
-        }
-    }
-
-    write(chunk:string){
-        if(this._server){
-            this._server.write(chunk)
-        } else {
-            this._body += chunk;
-        }
-    }
-
-    redirect(url: string): void {
-        if(this._server){
-            this._server.writeHead(302, {Location: url});
-        }
-
-        this._redirect = url;
-    }
-
-    get redirected():boolean {
-        return this._redirect !== undefined;
-    }
-
-    /** Was Nothing Sent
+    /** Stringify Encoded Data
      * 
-     * @returns {boolean}
+     * @param {any} data 
+     * @param {string} encode 
+     * @returns {string}
      */
-    get headersSent():boolean {
-        if(this._server)
-            return this._server.headersSent
+    private static _stringify(data:any, encode:string):string {
 
-        return this._headers.entries.length > 0 || this._body.length > 0;
+        //Test if not string.
+        if(typeof data !== "string") {
+
+            //Test if it has toString method
+            if(typeof data.toString === "function") {
+
+                //Test if accepts encdoding.
+                if(data.toString.length >= 1){
+                    return data.toString(encode);
+                } else {
+                    if(encode)
+                        console.warn(`Encoding '${encode}' ignored!`);
+                    return data.toString();
+                }
+            }
+
+            return String(data);
+        }
+        
+        return data;
+    }
+    
+    /** Text Encoder
+     * 
+     * @param {any} data 
+     * @param {string} encode 
+     * @returns {Uint8Array}
+     */
+    private static _text(data:any, encode:string):Uint8Array {
+        return OutgoingResponse.encoder.encode(OutgoingResponse._stringify(data, encode));
     }
 
-    end():ResponseInit{
-        if(this._server){
-            this._server.end();
+    /** Proto Response Constructor
+     * 
+     */
+    constructor(response?:ServerResponse){
+        super();
+
+        this.#status = 200;
+        this.#headers = new Map();
+        this.#body = [];
+        this.#server = response;
+
+        //Default headers
+        this.#headers.set("Referrer-Policy", "strict-origin");
+    }
+
+    /** Transform Data Override
+     * 
+     * @param {any} data 
+     * @param {string} encode 
+     * @param {Function} callback 
+     */
+    _transform(data:any, encode:string, callback:TransformCallback){
+        this.working = true;
+        if( data instanceof Uint8Array )
+            this.#body.push(data);
+        else
+            this.#body.push(OutgoingResponse._text(data, encode));
+        callback();
+    }
+
+    /** Flush Override
+     * 
+     * @param callback 
+     */
+    _flush(callback: TransformCallback): void {
+        this.working = false;
+        callback()
+    }
+
+    /** Has Commited
+     * 
+     * @returns 
+     */
+    commited():boolean{
+        return this.working !== undefined
+    }
+
+    /** Status Getter
+     * 
+     */
+    get status():number {
+        return this.#status;
+    }
+
+    /** Status Setter
+     * 
+     */
+    set status(value:number){
+        this.#status = value;
+    }
+
+    /** Headers Getter
+     * 
+     */
+    get headers():Map<string, string>{
+        return this.#headers;
+    }
+
+    /** Flush Response
+     * 
+     * @returns {Promise<Response>}
+     */
+    async flush():Promise<Response|undefined> {
+        while(this.working)
+            sleep();
+
+        if(this.#server){
+            if(this.#redirect){
+                this.#server.writeHead(this.#status, JSON.stringify({Location: this.#redirect}));
+            } else {
+                this.#server.statusCode = this.#status;
+                for(const [key, value] of this.#headers.entries()){
+                    this.#server.setHeader(key, value);
+                }
+                for(const chunk of this.#body)
+                    this.#server.write(chunk);
+            }
+            
+            return undefined;
         }
 
-        return [
-            this._body,
-            {
-                status: this._status || 200,
-                headers: this._headers
-            },
-            this._redirect
-        ]
+        if( this.#redirect && (this.#status > 300 && this.#status < 400) ){
+            return Response.redirect(this.#redirect, this.#status);
+        }
+
+        const headers:Record<string, string> = {};
+        for(const [name, value] of this.#headers.entries())
+            headers[name] = value;
+
+        return new Response(new Blob(this.#body), {
+            status: this.status,
+            headers: headers
+        });
+    }
+
+    /** Redirect Resposne
+     * 
+     * @param {URL} url 
+     * @param {number} status 
+     */
+    redirect(url:URL, status:number = 302){
+        this.#redirect = url;
+        this.#status = status;
+        this.working = false;
     }
 }
-
