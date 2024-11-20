62cb5ad64f4b03bf0d20e0ad5c01a97231ddb2ee
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-11-20 15:58:54 -0600

    Created empty, another single type.

--- src/Types/Empty.ts
+++ src/Types/Empty.ts
@@ -2,11 +2,27 @@
  * 
  * @author Alex Malotky
  */
+import { Validator } from ".";
 
+//Empty Type
 type Empty = null|undefined;
 export default Empty;
 
-export type EmptyType = "Empty";
+//Empty Format Name
+export  type EmptyType = "Empty";
+export const EmptyName = "Empty";
+
+/** Empty Validator
+ * 
+ */
+export class EmptyValidator extends Validator<Empty> {
+    constructor(value:unknown) {
+        if(isEmpty(value) === false)
+            throw new TypeError(`${value} is not empty!`);
+
+        super(EmptyName, null);
+    }
+}
 
 /** is Empty
  * 
