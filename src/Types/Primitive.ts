cd2c5e87423cab878d5667112c0a64690928af8b
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-11-20 15:59:27 -0600

    Deleted old primitive file.

--- src/Types/Primitive.ts
+++
@@ -1,9 +0,0 @@
-/** /Types/Primitive
- * 
- * @author Alex Malotky
- */
-type Primitive = number|boolean|string;
-export default Primitive;
-
-export type PrimitiveType = "number"|"boolean"|"string";
-
