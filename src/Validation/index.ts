dbd5b607a982e43a38777e47d783b8d43aa0cb76
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-11-22 09:36:41 -0600

    Fixed/ Re-Formated Validation File.

--- src/Validation/index.ts
+++ src/Validation/index.ts
@@ -1,62 +1,67 @@
-/*import {TypeClass, BasicNames, TypeNames, BASIC_NAMES} from "./Validation";
-import { BooleanType } from "./Validation/Boolean";
-import Color, { ColorType } from "./Validation/Color";
-import Date, { DateType } from "./Validation/Date";
-import DateTime, { DateTimeType } from "./Validation/DateTime";
-import Email, { EmaiType } from "./Validation/Email";
-import File, { FileType } from "./Validation/File";
-import { NumberType } from "./Validation/Number";
-import { StringType } from "./Validation/String";
-import Telephone, { TelephoneType } from "./Validation/Telephone";
-import Time, { TimeType } from "./Validation/Time";
-import Url, { UrlType } from "./Validation/Url";
-//import { ObjectType } from "./Types/Object";
+/** /Validation
+ * 
+ * @author Alex Malotky
+ */
+import {Color, Date, DateTime, Email, Telephone, Time, Url, TypeValidator} from "./Type";
+import BooleanValidator from "./Boolean";
+import NumberValidator from "./Number";
+import StringValidator from "./String";
+import ColorValidator from "./Color";
+import DateValidator from "./Date";
+import DateTimeValidator from "./DateTime";
+import EmailValidator from "./Email";
+import FileValidator from "./File";
+import TelephoneValidator from "./Telephone";
+import TimeValidator from "./Time";
+import UrlValidator from "./Url";
 //import { ListType } from "./Types/List";
-import { EmptyType } from "./Validation/Empty";
+//import { ObjectType } from "./Types/Object";
+import EmptyValidator from "./Empty";
+
+export default {
+    boolean:   (defaultValue?:boolean) =>new BooleanValidator(defaultValue),
+    number:    (defaultValue?:number)  =>new NumberValidator(defaultValue),
+    string:    (defaultValue?:string)  =>new StringValidator(defaultValue),
+    Color:     (defaultValue?:Color)   =>new ColorValidator(defaultValue),
+    Date:      (defaultValue?:Date)    =>new DateValidator(defaultValue),
+    DateTime:  (defaultValue?:DateTime)=>new DateTimeValidator(defaultValue),
+    Email:     (defaultValue?:Email)   =>new EmailValidator(defaultValue),
+    Telephone: (defaultValue?:Telephone)=>new TelephoneValidator(defaultValue),
+    Time:      (defaultValue?:Time)=>new TimeValidator(defaultValue),
+    Url:       (defaultValue?:Url)=>new UrlValidator(defaultValue),
+    File:      ()=>new FileValidator(),
+    //List:      (props:TypeClass<any>[], defaultValue?:boolean)=>new ListType(props[0]),
+    //Object:    (props:Dictionary<TypeClass<any>>, defaultValue?:boolean)=>new ObjectType(props),
+    //Optional: (type:TypeClass<any>)
+}
+
+export type TypeOf<T extends TypeValidator<any>> = T["_type"];
 
 /** Basic Types Map
  * 
  * Can hopefully use this to create a function that takes easier to read objects.
  */
 /*const Basic_Type_Map = {
-    "boolean": BooleanType,
-    "number": NumberType,
-    "string": StringType,
-    "Color": ColorType,
-    "Date": DateType,
-    "DateTime": DateTimeType,
-    "Email": EmaiType,
-    "File": FileType,
-    "Telephone": TelephoneType,
-    "Time": TimeType,
-    "Url": UrlType
+    "boolean": BooleanValidator,
+    "number": NumberValidator,
+    "string": StringValidator,
+    "Color": ColorValidator,
+    "Date": DateValidator,
+    "DateTime": DateTimeValidator,
+    "Email": EmailValidator,
+    "File": FileValidator,
+    "Telephone": TelephoneValidator,
+    "Time": TimeValidator,
+    "Url": UrlValidator
 }
 type BasicTypes = typeof Basic_Type_Map;
 
-export default {
-    boolean:   (defaultValue?:boolean) =>new BooleanType(defaultValue),
-    number:    (defaultValue?:number)  =>new NumberType(defaultValue),
-    string:    (defaultValue?:string)  =>new StringType(defaultValue),
-    Color:     (defaultValue?:Color)   =>new ColorType(defaultValue),
-    Date:      (defaultValue?:Date)    =>new DateType(defaultValue),
-    DateTime:  (defaultValue?:DateTime)=>new DateTimeType(defaultValue),
-    Email:     (defaultValue?:Email)   =>new EmaiType(defaultValue),
-    Telephone: (defaultValue?:Telephone)=>new TelephoneType(defaultValue),
-    Time:      (defaultValue?:Time)=>new TimeType(defaultValue),
-    Url:       (defaultValue?:Url)=>new UrlType(defaultValue),
-    File:      ()=>new FileType(),
-    //List:      (props:TypeClass<any>[], defaultValue?:boolean)=>new ListType(props[0]),
-    //Object:    (props:Dictionary<TypeClass<any>>, defaultValue?:boolean)=>new ObjectType(props),
-    //Optional: (type:TypeClass<any>)
-}
 
-export type TypeOf<T extends TypeClass<any>> = T["_type"];
 
-/** Basic Names Helper Array
- * 
- * Hopefully Am able to use this later
- */
-/*export const BASIC_NAMES = [
+
+
+//Basic Names Helper Array
+export const BASIC_NAMES = [
     BooleanName, NumberName, StringName,
     ColorName, DateTimeName, DateName, EmailName, FileName,  TelephoneName, TimeName, UrlName
 ] as const;
