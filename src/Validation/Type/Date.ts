7a7bfd0c1d69d34d91b2cad72a7f888715f9c0e8
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-11-22 12:29:25 -0600

    Actually format date value.

--- src/Validation/Type/Date.ts
+++ src/Validation/Type/Date.ts
@@ -2,7 +2,7 @@
  * 
  * @author Alex Malotky
  */
-const DATE_REGEX = /^(\d{4})-(\d{1,2})-(\d{1,2})$/;
+const DATE_REGEX = /^(\d{1,4})[\-\/\\.](\d{1,2})[\-\/\\.](\d{1,2})$/;
 
 // Date Type
 type Date = string;
@@ -19,10 +19,66 @@ export function formatDate(value:unknown):Date {
     if(typeof value !== "string")
         throw new TypeError("Date must be stored in a string!");
 
-    if(value.match(DATE_REGEX) === null)
+    const match = value.match(DATE_REGEX)
+    if( match === null)
         throw new TypeError("Date is not formated correctly!");
 
-    return value;
+    const y = Number(match[1]);
+    const m = Number(match[2]);
+    const d = Number(match[3]);
+
+    if(!validateDateParts(y, m, d))
+        throw new Error("Date is invalid!");
+
+    const year = `000${y}`.slice(-4);
+    const month = `0${m}`.slice(-2);
+    const day = `0${d}`.slice(-2);
+
+    return `${year}-${month}-${day}`;
+}
+
+/** Validate Date Parts
+ * 
+ * @param {number} year 
+ * @param {number} month 
+ * @param {number} day 
+ * @returns {boolean}
+ */
+export function validateDateParts(year:number, month:number, day: number):boolean {
+    if( day <= 0 || year <= 0)
+        return false;
+    
+    switch (month){
+        case 1:  //January
+        case 3:  //March
+        case 5:  //May
+        case 7:  //July
+        case 8:  //August
+        case 10: //October
+        case 12: //December
+            return day <= 31;
+
+        case 4:  //April
+        case 6:  //June
+        case 9:  //September
+        case 11: //November
+            return day <= 30;
+
+        case 2: //Febuary
+            if(isLeapYear(year))
+                return day <= 29;
+            return day <= 28;
+    }
+
+    return false;
+}
+
+/** Is Leap Year
+ * 
+ * @param {number} year 
+ */
+function isLeapYear(year:number):boolean {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 
 /** Is Date
