086ebde9ad0e2eff7812c6b71bc37f5fddbacee5
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-11-26 11:15:02 -0600

    Update export helpfer functions and added class that helps query builder.

--- src/Validation/index.ts
+++ src/Validation/index.ts
@@ -2,7 +2,7 @@
  * 
  * @author Alex Malotky
  */
-import Type, {Color, Date, DateTime, Email, Telephone, Time, Url, TypeValidator, List, Object} from "./Type";
+import Type, {Color, Date, DateTime, Email, Telephone, Time, Url, TypeValidator, List, Object, Simple} from "./Type";
 import BooleanValidator from "./Boolean";
 import NumberValidator from "./Number";
 import StringValidator from "./String";
@@ -18,62 +18,111 @@ import EmptyValidator from "./Empty";
 import ListValidator from "./List";
 import ObjectValidator, {ObjectProperties, ObjectDefaults} from "./Object";
 import OptionalValidator from "./Optional";
+import { isEmpty } from "./Type/Empty";
 
-export default {
-    //Basic Helper Functions
-    boolean:   (defaultValue?:boolean)  =>new BooleanValidator(defaultValue),
-    number:    (defaultValue?:number)   =>new NumberValidator(defaultValue),
-    string:    (defaultValue?:string)   =>new StringValidator(defaultValue),
-    Color:     (defaultValue?:Color)    =>new ColorValidator(defaultValue),
-    Date:      (defaultValue?:Date)     =>new DateValidator(defaultValue),
-    DateTime:  (defaultValue?:DateTime) =>new DateTimeValidator(defaultValue),
-    Email:     (defaultValue?:Email)    =>new EmailValidator(defaultValue),
-    Telephone: (defaultValue?:Telephone)=>new TelephoneValidator(defaultValue),
-    Time:      (defaultValue?:Time)     =>new TimeValidator(defaultValue),
-    Url:       (defaultValue?:Url)      =>new UrlValidator(defaultValue),
-    Empty:     ()=>new EmptyValidator(),
-    File:      ()=>new FileValidator(),
-    // Complex Helper Functions
-    List: function <T extends Type>(type:TypeValidator<T>, seperator?:string, defaultValue?:List<T>):ListValidator<T>{
-        return new ListValidator(type, seperator, defaultValue)
-    },
-    Object: function <P extends ObjectProperties>(properties:P, defaultValue?:ObjectDefaults<keyof P>):ObjectValidator<P> {
-        return new ObjectValidator(properties, defaultValue);
-    },
-    Optional: function <T extends Type>(type:TypeValidator<T>, defaultValue?:T|null):OptionalValidator<T> {
-        return new OptionalValidator(type, defaultValue)
-    }
+//Basic Helper Functions
+export function boolean  (defaultValue?:boolean)  { return new BooleanValidator(defaultValue) };
+export function number   (defaultValue?:number)   { return new NumberValidator(defaultValue) };
+export function string   (defaultValue?:string)   {return new StringValidator(defaultValue) };
+export function Color    (defaultValue?:Color)    { return new ColorValidator(defaultValue) };
+export function Date     (defaultValue?:Date)     { return new DateValidator(defaultValue) };
+export function DateTime (defaultValue?:DateTime) { return new DateTimeValidator(defaultValue) };
+export function Email    (defaultValue?:Email)    { return new EmailValidator(defaultValue) };
+export function Telephone(defaultValue?:Telephone){ return new TelephoneValidator(defaultValue) };
+export function Time     (defaultValue?:Time)     { return new TimeValidator(defaultValue) };
+export function Url      (defaultValue?:Url)      { return new UrlValidator(defaultValue) };
+export function Empty    () { return new EmptyValidator() };
+export function File     () { return new FileValidator() };
+
+//Complex Helper Functions
+export function List<T extends Type>(type:TypeValidator<T>, seperator?:string, defaultValue?:List<T>):ListValidator<T>{
+    return new ListValidator(type, seperator, defaultValue)
+}
+export function Object<P extends ObjectProperties>(properties:P, defaultValue?:ObjectDefaults<keyof P>):ObjectValidator<P> {
+    return new ObjectValidator(properties, defaultValue);
+}
+export function Optional<T extends Type>(type:TypeValidator<T>, defaultValue?:T|null):OptionalValidator<T> {
+    return new OptionalValidator(type, defaultValue)
 }
 
 //Get Type From Validator
 export type TypeOf<T extends TypeValidator<Type>> = T["_type"];
 
-/** Basic Types Map
+export type DataConstraints<K extends string|number|symbol> = ObjectDefaults<K>;
+export type {ObjectProperties}
+
+/** Data Object 
  * 
- * Can hopefully use this to create a function that takes easier to read objects.
  */
-/*const Basic_Type_Map = {
-    "boolean": BooleanValidator,
-    "number": NumberValidator,
-    "string": StringValidator,
-    "Color": ColorValidator,
-    "Date": DateValidator,
-    "DateTime": DateTimeValidator,
-    "Email": EmailValidator,
-    "File": FileValidator,
-    "Telephone": TelephoneValidator,
-    "Time": TimeValidator,
-    "Url": UrlValidator
+export default class DataObject<P extends ObjectProperties> extends ObjectValidator<P> {
+    private _table:string;
+    private _props:P;
+
+    constructor(tableName:string, properties:P) {
+        super(properties);
+        this._table = tableName;
+        this._props = properties;
+    }
+
+    /** Get Table Name
+     * 
+     */
+    get name():string {
+        return this._table;
+    }
+
+    /** Build Constraints
+     * 
+     * @param {DataConstraints} constraints 
+     * @returns {Array}
+     */
+    buildConstraints(constraints?:DataConstraints<keyof P>):[string, Simple[]]{
+        if(isEmpty(constraints))
+            return ["", []];
+
+        let string = "WHERE ";
+        const values:Array<Simple> = []
+        for(const name in constraints) {
+            string += name + " = ? AND"
+            values.push(this._props[name].simplify(constraints[name]));
+        }
+
+        return [string.slice(-3), values];
+    }
+
+    /** Build Insert
+     * 
+     * @param {Object} value 
+     * @returns {Array}
+     */
+    buildInsertValues(value:Object<keyof P>):[string, Simple[]]{
+        let queryNames:string = "(";
+        let queryValues:string = "VALUES(";
+        const values:Array<Simple> = [];
+
+        for(const name in value) {
+            queryNames  += name + ", ";
+            queryValues += "?, ";
+            values.push(this._props[name].simplify(value[name]));
+        }
+
+        return [`${queryNames.slice(-2)}) ${queryValues.slice(-2)})`, values]
+    }
+
+    /** Build Update
+     * 
+     * @param {Object} value 
+     * @returns {Array}
+     */
+    buildUpdateValues(value:Object<keyof P>):[string, Simple[]] {
+        let string:string = "SET ";
+        const values:Array<Simple> = [];
+
+        for(const name in value) {
+            string  += name + " = ?, ";
+            values.push(this._props[name].simplify(value[name]));
+        }
+
+        return [string, values];
+    }
 }
-type BasicTypes = typeof Basic_Type_Map;
-
-//Basic Names Helper Array
-export const BASIC_NAMES = [
-    BooleanName, NumberName, StringName,
-    ColorName, DateTimeName, DateName, EmailName, FileName,  TelephoneName, TimeName, UrlName
-] as const;
-
-//Name Types
-export type BasicNames = typeof BASIC_NAMES[number]
-export type TypeNames  = BasicNames | Dictionary<BasicNames> | Array<BasicNames>;
-*/
