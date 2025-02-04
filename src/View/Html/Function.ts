26fea1ef0a9c287aa07a44405c2493279665871c
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-02-04 16:14:53 -0600

    Added to content that handles mangle better then previous methods;

--- src/View/Html/Function.ts
+++ src/View/Html/Function.ts
@@ -12,8 +12,19 @@
  * @param {Function} fun 
  * @returns {string}
  */
-export function toString(fun:Function):string {
+export function toAttribute(fun:Function):string {
     const str = ""+fun;
     return str.substring(str.indexOf("{")+1, str.lastIndexOf("}")-1)
               .replace(/\s+/g, " ").trim();
+}
+
+export function toContent(fun:Function):string {
+    const str = ""+fun;
+    const match = str.match(/^(?:(?:(?:async\s+)?function\s+(\w+)\(.*?\)\s*)|(?:const\s+(\w+)\s+=\s+(?:async)?\(.*?\)\s+=>\s+)){/i);
+    if(match === null){
+        throw new Error("Unable to find function name!")
+    }
+
+    const name = match[1] || match[2];
+    return str + `;${name}(env);`;
 }
