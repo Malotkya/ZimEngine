e46ef34479ae50b9a29a30a988053d310048a826
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-02-05 08:53:55 -0600

    Opted for fixed env variable name instead of finding function name and calling it.

--- src/View/Html/Function.ts
+++ src/View/Html/Function.ts
@@ -3,7 +3,7 @@
  * @author Alex Malotky
  */
 
-/** Convert Function to String
+/** Convert Function to Attibute String
  * 
  * Will convert a function to a string so that it can be executed on the front end.
  * 
@@ -18,13 +18,17 @@ export function toAttribute(fun:Function):string {
               .replace(/\s+/g, " ").trim();
 }
 
+/** Convert Function to Content String
+ * 
+ * Will convert a function to a string so that it can be executed on the front end.
+ * 
+ * !! Does loose its scope !!
+ * 
+ * @param {Function} fun 
+ * @returns {string}
+ */
 export function toContent(fun:Function):string {
     const str = ""+fun;
-    const match = str.match(/^(?:(?:(?:async\s+)?function\s+(\w+)\(.*?\)\s*)|(?:const\s+(\w+)\s+=\s+(?:async)?\(.*?\)\s+=>\s+)){/i);
-    if(match === null){
-        throw new Error("Unable to find function name!")
-    }
-
-    const name = match[1] || match[2];
-    return str + `;${name}(env);`;
+    return str.substring(str.indexOf("{")+1, str.lastIndexOf("}")-1)
+            .replaceAll(/(?<=\s|;)env\w+/ig, "env").trim();
 }
